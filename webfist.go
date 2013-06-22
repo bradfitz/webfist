2890cd1616d7e186bb454650e562147c479130bb
Brett Slatkin <bslatkin@gmail.com>
2013-06-22 11:10:00 -0700

    Log the part it's running on

--- webfist.go
+++ webfist.go
@@ -43,6 +43,7 @@ func main() {
 			WriteTimeout: 5 * time.Minute,
 		},
 	}
+	log.Printf("Started: webserver %s, SMTP %s", webAddr, smtpAddr)
 	go srv.runSMTP(smtpln)
 	log.Fatal(srv.httpServer.Serve(webln))
 }
